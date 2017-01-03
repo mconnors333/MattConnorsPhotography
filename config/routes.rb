f472aac647cd4aa4b960cf672ca714647e06834d
mconnors333 <matthew.connors333@gmail.com>
2017-01-03 15:18:52 -0500

    Adding static pages to my site

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "albums#index"
   resources :users
   resources :favorites
   resources :photos
   resources :albums
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/pages/:page" => "pages#show"
+  root "pages#show", page: "home"
 end
