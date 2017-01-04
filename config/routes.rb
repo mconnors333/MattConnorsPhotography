34a8e582c694b680d8e0b13717d037694d166518
mconnors333 <matthew.connors333@gmail.com>
2017-01-04 11:03:22 -0500

    adding bootstrap to my project, hopefully

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :photos
   resources :albums
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "pages#home" 
+  root "pages#home"
   get '/home' => 'pages#home'
   get '/about' => 'pages#about'
   get '/contact' => 'pages#contact'
