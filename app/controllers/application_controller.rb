97218851e30dcde8b473867fc99966f93829a725
mconnors333 <matthew.connors333@gmail.com>
2017-01-05 11:50:43 -0500

    about to add bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+      def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :email, :password, :password_confirmation])
+        devise_parameter_sanitizer.permit(:account_update) { |u|
+          u.permit(:first_name, :last_name, :phone_number, :password, :password_confirmation, :current_password)
+        }
+      end
+
 end
