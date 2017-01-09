1afae39a5d6b4fae95a225bc60d501aa66d32be4
mconnors333 <matthew.connors333@gmail.com>
2017-01-09 11:54:18 -0500

    added redirect for signed in users to their albums page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,7 @@ class ApplicationController < ActionController::Base
         }
       end
 
+      def after_sign_in_path_for(resource)
+        stored_location_for(resource) || albums_path
+      end
 end
